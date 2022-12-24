4597d41a988e947df0d3cf878dbc3ec3699183a6
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2022-12-24 20:50:31 -0300

    feat: add some dom helper functions

--- utils/javascript/dom_utils.js
+++ utils/javascript/dom_utils.js
@@ -1,17 +1,34 @@
 // Helper for querySelector / querySelectorAll
-const select = (el, isAll = false) => {
+const selectElement = (el, isAll = false) => {
   if (typeof el === 'string') {
     return isAll ? document.querySelectorAll(el) : document.querySelector(el);
   }
 
-  return element;
+  return el;
 };
 
 // Helper for createElement
-const create = el => document.createElement(el);
+const createElement = el => document.createElement(el);
 
 
 // Helper for appendChild
-const append = (parent, child) => {
-  return parent.append(child);
-}
+const appendChild = (parent, child) => parent.append(child);
+
+
+// Helper for classList
+const hasClass = (el, className) => el.classList.contains(className);
+
+// Helper for classList
+const toggleClass = (el, className) => el.classList.toggle(className);
+
+// Helper for classList
+const removeClass = (element, className) => element.classList.remove(className);
+
+// Helper for getAttribute
+const getAttribute = (el, attribute) => el.getAttribute(attribute);
+
+// Helper setAttribute
+const setAttribute = (el, attribute, value) => el.setAttribute(attribute, value);
+
+// Helper for removeAttribute
+const removeAttribute = (el, attribute) => el.removeAttribute(attribute);
