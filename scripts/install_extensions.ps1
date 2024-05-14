a417820c98555a0359712c936c86f6ecc6c6a48b
Silas Rodrigues <silasrodrigues.fatec@gmail.com>
2024-05-13 23:17:10 -0300

    refactor: update shell script to install extensions

--- scripts/install_extensions.ps1
+++ scripts/install_extensions.ps1
@@ -1,62 +1,141 @@
 $null = $host.UI.RawUI.FlushInputBuffer()
 
-$vscodePath = Get-Command "code" -ErrorAction SilentlyContinue
+function Check-VSCodeInstallation {
+    $vscodePath = Get-Command "code" -ErrorAction SilentlyContinue
 
-if (-not $vscodePath) {
-    Write-Host -ForegroundColor Red "Visual Studio Code is not installed. Please install it before running this script"
-    exit 1
-}
-
-if (-not (Get-Module -ListAvailable -Name PSReadline)) {
-    Install-Module -Name PSReadline -Force -Scope CurrentUser
-}
-
-Import-Module PSReadline
-
-$fileName = "README"
-$folderName = ".vscode"
-$fileExtension = ".md"
-$filePath = ".\$folderName\$fileName$fileExtension"
-
-$forceUpdate = $args.Contains("--force")
-
-$fileContent = Get-Content -Path $filePath
-
-$regex = '(https:\/\/marketplace\.visualstudio\.com\/items\?itemName=[^(\s)]+)'
-$matches = [regex]::Matches($fileContent -join "`n", $regex)
-
-if ($matches.Count -eq 0) {
-    Write-Host -ForegroundColor Yellow "No extension link found in the file in $filePath. Make sure this file and $filePath are in the same directory"
-    exit 1
-}
-
-foreach ($match in $matches) {
-    $extensionURL = $match.Groups[1].Value
-
-    $extensionID = $extensionURL -replace '.+\/items\?itemName=(.+)', '$1'
-
-    $isInstalled = & $vscodePath --list-extensions | Where-Object { $_ -eq $extensionID }
-
-    if (-not $isInstalled -or $forceUpdate) {
-        $null = & $vscodePath --install-extension $extensionID --force
-        $status = if ($forceUpdate) { "updated with --force flag" } else { "installed" }
-        Write-Host -ForegroundColor Green "Extension '$extensionID' $status."
-    } else {
-        Write-Host -ForegroundColor Yellow "Extension '$extensionID' already installed."
+    if (-not $vscodePath) {
+        Write-Host -ForegroundColor Red "Visual Studio Code is not installed. Please install it before running this script"
+        exit 1
     }
 }
 
-$padding = " " * 4
-$border = "+" + "-" * 66 + "+"
-$content = @"
+function Install-PSReadlineModule {
+    if (-not (Get-Module -ListAvailable -Name PSReadline)) {
+        Install-Module -Name PSReadline -Force -Scope CurrentUser
+    }
+}
+
+function Get-FolderAndFileName {
+    Show-Message "Enter the folder name and file name (e.g., FolderName/FileName)" "Green"
+    $folderAndFileName = Read-Host
+    return $folderAndFileName -split "/"
+}
+
+function Get-FileExtension {
+    param (
+        [string]$folderName,
+        [string]$fileName
+    )
+
+    $mdFilePath = ".\$folderName\$fileName.md"
+    $mdFileExists = Test-Path $mdFilePath
+
+    $txtFilePath = ".\$folderName\$fileName.txt"
+    $txtFileExists = Test-Path $txtFilePath
+
+    if ($mdFileExists) {
+        return ".md"
+    } elseif ($txtFileExists) {
+        return ".txt"
+    } else {
+        Show-Message "No file with .md or .txt extension found in the specified directory" "Red"
+        exit 1
+    }
+}
+
+function Get-ExtensionURLs {
+    param (
+        [string]$filePath
+    )
+
+    $fileContent = Get-Content -Path $filePath
+
+    $regex = '(https:\/\/marketplace\.visualstudio\.com\/items\?itemName=[^(\s)]+)'
+    $matches = [regex]::Matches($fileContent -join "`n", $regex)
+
+    if ($matches.Count -eq 0) {
+        $fullFileName = "$folderName\$fileName$fileExtension"
+        Show-Message "No extension link found in the file in $fullFileName. Make sure you have entered the correct directory" "Yellow"
+        exit 1
+    }
+
+    $extensionURLs = @()
+    foreach ($match in $matches) {
+        $extensionURLs += $match.Groups[1].Value
+    }
+
+    return $extensionURLs
+}
+
+function Install-Extensions {
+    param (
+        [string]$vscodePath,
+        [string[]]$extensionURLs,
+        [bool]$forceUpdate
+    )
+
+    foreach ($extensionURL in $extensionURLs) {
+        $extensionID = $extensionURL -replace '.+\/items\?itemName=(.+)', '$1'
+
+        $isInstalled = & $vscodePath --list-extensions | Where-Object { $_ -eq $extensionID }
+
+        if (-not $isInstalled -or $forceUpdate) {
+            $null = & $vscodePath --install-extension $extensionID --force
+            $status = if ($forceUpdate) { "updated with --force flag" } else { "installed" }
+            Write-Host -ForegroundColor Green "Extension '$extensionID' $status."
+        } else {
+            Write-Host -ForegroundColor Yellow "Extension '$extensionID' already installed."
+        }
+
+        Write-Host ""
+    }
+}
+
+function Show-Message {
+    param (
+        [string]$message,
+        [string]$color
+    )
+
+    $border = "+" + "-" * ($message.Length + 4) + "+"
+    Write-Host $border -ForegroundColor $color
+    Write-Host "| $message |" -ForegroundColor $color
+    Write-Host $border -ForegroundColor $color
+}
+
+# Função principal
+function Main {
+    Check-VSCodeInstallation
+
+    Install-PSReadlineModule
+
+    $folderName, $fileName = Get-FolderAndFileName
+
+    $fileExtension = Get-FileExtension -folderName $folderName -fileName $fileName
+
+    $filePath = ".\$folderName\$fileName$fileExtension"
+
+    $extensionURLs = Get-ExtensionURLs -filePath $filePath
+
+    $vscodePath = Get-Command "code" -ErrorAction SilentlyContinue
+    $forceUpdate = $args.Contains("--force")
+
+    Install-Extensions -vscodePath $vscodePath -extensionURLs $extensionURLs -forceUpdate $forceUpdate
+
+    $padding = " " * 4
+    $border = "+" + "-" * 66 + "+"
+    $content = @"
 |                                                                   |
 |  The install command has been executed successfully.              |
 |                                                                   |
 "@
-$styledMessage = @"
+    $styledMessage = @"
 $border
 $content
 $border
 "@
 
-Write-Host $styledMessage -ForegroundColor DarkGreen
+    Write-Host $styledMessage -ForegroundColor DarkGreen
+}
+
+Main
